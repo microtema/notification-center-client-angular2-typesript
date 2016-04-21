20bbb197bcf3962ffe853b695cda577a8c398c78
Mario Tema <Mario.Tema@gmx.de>
2016-04-21 20:56:04 +0200

    First draft

---
+++ app/script/main.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    template: '<div>go for it...</div>'
+})
+export class AppComponent {
+    title = 'Notification Message Center';
+}
